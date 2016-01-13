3f54f976e04168af47e9826c19d80dd52f9e60e9
Charles Elvira <ninou14fr@ufl.edu>
2016-01-13 16:22:56 -0500

    Final Bubble sort

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -1,43 +1,34 @@
 #include<iostream>
-void bubbleSort(int *listOfInteger, int size)
+void bubbleSort(int listOfInteger[], int size)
 {
-	bool swap = true;
-	int temp=0;
-	while(swap)
-	{	
-		for(int i = 0 ; i+1 < size; i++)
+		int temp;
+		for(int i = 0 ; i < size - 1; i++)
 		{
-			if(listOfInteger[i] > listOfInteger[i+1])
+			for(int j = 0; j< size - i - 1; j++)
 			{
-				temp = listOfInteger[i];
-				listOfInteger[i] = listOfInteger[i+1];
-				listOfInteger[i+1] = temp;
-				swap = true;
 
-				for(int i = 0; i<9;i++)
-       				{
-			                std::cout<<listOfInteger[i]<<" ";
-			        }
-				std::cout<<std::endl;
+				if(listOfInteger[j] > listOfInteger[j+1])
+				{
+					temp = listOfInteger[j];
+					listOfInteger[j] = listOfInteger[j+1];
+					listOfInteger[j+1] = temp;
 				
+				}
 			}
-			else if(listOfInteger[i] < listOfInteger[i+1])
-			{
-				swap = false;
-			}
-		
+			
 		}
-	}
+		
 }
+
 int main(int,char*[])
 {
         clock_t t;
 
-        int list[] =  {9,2,3,4,5,6,32,98,1};
+        int list[] =  {9,2,3,4,5,6,32,98,11,10,35,1};
         t = clock();
-        bubbleSort(list,9);
+        bubbleSort(list,11);
         std::cout<<"It took : "<<((float)t)/CLOCKS_PER_SEC<<std::endl;
-        for(int i = 0; i<9;i++)
+        for(int i = 0; i<11;i++)
         {
                 std::cout<<list[i]<<" ";
         }	
